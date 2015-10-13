e0aefa6d9509bbba7b42e91d6340304b601f8fdf
DidierStevens <didier.stevens@gmail.com>
2015-10-13 22:09:22 +0200

    20151013a

---
+++ cut-bytes.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python
+
+__description__ = 'Cut a section of bytes out of a file'
+__author__ = 'Didier Stevens'
+__version__ = '0.0.1'
+__date__ = '2015/09/13'
+
+"""
+
+Source code put in public domain by Didier Stevens, no Copyright
+https://DidierStevens.com
+Use at your own risk
+
+History:
+  2015/09/13: start
+
+Todo:
+"""
+
+import optparse
+import textwrap
+import re
+import cStringIO
+import sys
+import os
+
+MALWARE_PASSWORD = 'infected'
+
+def PrintManual():
+    manual = '''
+Manual:
+
+cut-bytes.py reads a file (from disk, from stdin or from inside a (password protected) ZIP file) and outputs it (partially) to stdout.
+cut-bytes.py can be instructed via a cut-expression to only send partial input to stdout.
+
+The cut-expression allows for the partial selection of a stream. Use this expression to "cut out" part of the stream.
+This cut-expression is composed of 2 terms separated by a colon (:), like this:
+termA:termB
+termA and termB can be:
+- nothing (an empty string)
+- a positive number; example: 10
+- an hexadecimal number (to be preceded by 0x); example: 0x10
+- a case sensitive string to search for (surrounded by square brackets and single quotes); example: ['MZ']
+- an hexadecimal string to search for (surrounded by square brackets); example: [d0cf11e0]
+If termA is nothing, then the cut section of bytes starts with the byte at position 0.
+If termA is a number, then the cut section of bytes starts with the byte at the position given by the number (first byte has index 0).
+If termA is a string to search for, then the cut section of bytes starts with the byte at the position where the string is first found. If the string is not found, the cut is empty (0 bytes).
+If termB is nothing, then the cut section of bytes ends with the last byte.
+If termB is a number, then the cut section of bytes ends with the byte at the position given by the number (first byte has index 0).
+When termB is a number, it can have suffix letter l. This indicates that the number is a length (number of bytes), and not a position.
+If termB is a string to search for, then the cut section of bytes ends with the last byte at the position where the string is first found. If the string is not found, the cut is empty (0 bytes).
+No checks are made to assure that the position specified by termA is lower than the position specified by termB. This is left up to the user.
+Examples:
+This cut-expression can be used to dump the first 256 bytes of a PE file located inside the stream: ['MZ']:0x100l
+This cut-expression can be used to dump the OLE file located inside the stream: [d0cf11e0]:
+'''
+    for line in manual.split('\n'):
+        print(textwrap.fill(line))
+
+#Convert 2 Bytes If Python 3
+def C2BIP3(string):
+    if sys.version_info[0] > 2:
+        return bytes([ord(x) for x in string])
+    else:
+        return string
+
+def IfWIN32SetBinary(io):
+    if sys.platform == 'win32':
+        import msvcrt
+        msvcrt.setmode(io.fileno(), os.O_BINARY)
+
+CUTTERM_NOTHING = 0
+CUTTERM_POSITION = 1
+CUTTERM_FIND = 2
+CUTTERM_LENGTH = 3
+
+def ParseCutTerm(argument):
+    if argument == '':
+        return CUTTERM_NOTHING, None, ''
+    oMatch = re.match(r'0x([0-9a-f]+)', argument, re.I)
+    if oMatch == None:
+        oMatch = re.match(r'(\d+)', argument)
+    else:
+        return CUTTERM_POSITION, int(oMatch.group(1), 16), argument[len(oMatch.group(0)):]
+    if oMatch == None:
+        oMatch = re.match(r'\[([0-9a-f]+)\]', argument, re.I)
+    else:
+        return CUTTERM_POSITION, int(oMatch.group(1)), argument[len(oMatch.group(0)):]
+    if oMatch == None:
+        oMatch = re.match(r"\[\'(.+)\'\]", argument)
+    else:
+        if len(oMatch.group(1)) % 2 == 1:
+            raise
+        else:
+            return CUTTERM_FIND, binascii.a2b_hex(oMatch.group(1)), argument[len(oMatch.group(0)):]
+    if oMatch == None:
+        return None, None, argument
+    else:
+        return CUTTERM_FIND, oMatch.group(1), argument[len(oMatch.group(0)):]
+
+def ParseCutArgument(argument):
+    type, value, remainder = ParseCutTerm(argument.strip())
+    if type == CUTTERM_NOTHING:
+        return CUTTERM_NOTHING, None, CUTTERM_NOTHING, None
+    elif type == None:
+        if remainder.startswith(':'):
+            typeLeft = CUTTERM_NOTHING
+            valueLeft = None
+            remainder = remainder[1:]
+        else:
+            return None, None, None, None
+    else:
+        typeLeft = type
+        valueLeft = value
+        if remainder.startswith(':'):
+            remainder = remainder[1:]
+        else:
+            return None, None, None, None
+    type, value, remainder = ParseCutTerm(remainder)
+    if type == CUTTERM_POSITION and remainder == 'l':
+        return typeLeft, valueLeft, CUTTERM_LENGTH, value
+    elif type == None or remainder != '':
+        return None, None, None, None
+    else:
+        return typeLeft, valueLeft, type, value
+
+def CutData(stream, cutArgument):
+    if cutArgument == '':
+        return stream
+
+    typeLeft, valueLeft, typeRight, valueRight = ParseCutArgument(cutArgument)
+
+    if typeLeft == None:
+        return stream
+
+    if typeLeft == CUTTERM_NOTHING:
+        positionBegin = 0
+    elif typeLeft == CUTTERM_POSITION:
+        positionBegin = valueLeft
+    else:
+        positionBegin = stream.find(valueLeft)
+        if positionBegin == -1:
+            return ''
+
+    if typeRight == CUTTERM_NOTHING:
+        positionEnd = len(stream)
+    elif typeRight == CUTTERM_POSITION:
+        positionEnd = valueRight + 1
+    elif typeRight == CUTTERM_LENGTH:
+        positionEnd = positionBegin + valueRight
+    else:
+        positionEnd = stream.find(valueRight)
+        if positionEnd == -1:
+            return ''
+        else:
+            positionEnd += len(valueRight)
+
+    return stream[positionBegin:positionEnd]
+
+#Fix for http://bugs.python.org/issue11395
+def StdoutWriteChunked(data):
+    while data != '':
+        sys.stdout.write(data[0:10000])
+        try:
+            sys.stdout.flush()
+        except IOError:
+            return
+        data = data[10000:]
+
+def CutBytes(expression, filename, options):
+    if filename == '':
+        IfWIN32SetBinary(sys.stdin)
+        oStringIO = cStringIO.StringIO(sys.stdin.read())
+    elif filename.lower().endswith('.zip'):
+        oZipfile = zipfile.ZipFile(filename, 'r')
+        oZipContent = oZipfile.open(oZipfile.infolist()[0], 'r', C2BIP3(MALWARE_PASSWORD))
+        oStringIO = cStringIO.StringIO(oZipContent.read())
+        oZipContent.close()
+        oZipfile.close()
+    else:
+        oStringIO = cStringIO.StringIO(open(filename, 'rb').read())
+
+    IfWIN32SetBinary(sys.stdout)
+    StdoutWriteChunked(CutData(oStringIO.read(), expression))
+
+def Main():
+    oParser = optparse.OptionParser(usage='usage: %prog [options] cut-expression [file]\n' + __description__, version='%prog ' + __version__)
+    oParser.add_option('-m', '--man', action='store_true', default=False, help='Print manual')
+    (options, args) = oParser.parse_args()
+
+    if options.man:
+        oParser.print_help()
+        PrintManual()
+        return
+
+    if len(args) == 0 or len(args) > 2:
+        oParser.print_help()
+        print('')
+        print('  Source code put in the public domain by Didier Stevens, no Copyright')
+        print('  Use at your own risk')
+        print('  https://DidierStevens.com')
+        return
+
+    if ParseCutArgument(args[0])[0] == None:
+        print('Error: the cut-expression is invalid: %s' % args[0])
+        return
+
+    if len(args) == 1:
+        CutBytes(args[0], '', options)
+    else:
+        CutBytes(args[0], args[1], options)
+
+if __name__ == '__main__':
+    Main()
