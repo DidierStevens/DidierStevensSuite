0600b7b17f70b6aadcc59a32990457ced1eec11f
DidierStevens <didier.stevens@gmail.com>
2016-05-05 23:33:16 +0200

    20160505a

--- numbers-to-hex.py
+++ numbers-to-hex.py
@@ -2,8 +2,8 @@
 
 __description__ = "Program to convert decimal numbers into hex numbers"
 __author__ = 'Didier Stevens'
-__version__ = '0.0.2'
-__date__ = '2016/02/02'
+__version__ = '0.0.3'
+__date__ = '2016/05/03'
 
 """
 
@@ -15,6 +15,7 @@ History:
   2015/12/18: start
   2016/01/06: wrote man; added option -i
   2016/02/02: 0.0.2 bugfix
+  2016/05/03: 0.0.3 added option -n and -s
 
 Todo:
 """
@@ -30,7 +31,9 @@ def PrintManual():
     manual = '''
 Manual:
 
-This program reads lines from the given file(s) or standard input, and then extracts decimal numbers from each line. A decimal number is a sequence of digits. All numbers found in a line are converted to hexadecimal and outputed as a line. Hexadecimal numbers are separated by a space character. If a number is larger than 255/0xFF, an error is generated, except when option -i is used.
+This program reads lines from the given file(s) or standard input, and then extracts decimal numbers from each line. A decimal number is a sequence of digits (optionally prefixed with a dash - for negative numbers). All numbers found in a line are converted to hexadecimal and outputed as a line. Hexadecimal numbers are separated by a space character. If a number is smaller than 0 or larger than 255/0xFF, an error is generated, except when option -i is used.
+Option -s (--signed) indicates that the input numbers are signed bytes: -1 is 0xFF, -2 is 0xFE, ...
+Option -n NUMBER (--number) requires that at least NUMBER numbers are present in the input line (the default is 1 number).
 
 The hexadecimal numbers are written to standard output, except when option -o is used. When option -o is used, the numbers are written to the file specified by option -o.
 '''
@@ -118,7 +121,7 @@ def ProcessFile(fIn, fullread):
             yield line.strip('\n')
 
 def NumbersToHexSingle(filenames, oOutput, options):
-    oRE = re.compile('\d+')
+    oRE = re.compile('-?\d+')
     numberTooLarge = False
     for filename in filenames:
         if filename == '':
@@ -127,11 +130,18 @@ def NumbersToHexSingle(filenames, oOutput, options):
             fIn = open(filename, 'r')
         for line in ProcessFile(fIn, False):
             numbers = [int(number) for number in oRE.findall(line)]
-            numberTooLarge = max(numbers) > 255 and not options.ignore
-            if numberTooLarge:
-                print('Error: found number larger than 255: %d' % max(numbers))
-                break
-            oOutput.Line(' '.join(['%02x' % int(number) for number in oRE.findall(line)]))
+            if options.signed:
+            	  numbers = [IFF(number < 0, number + 256, number) for number in numbers]
+            if len(numbers) >= options.number:
+                numberTooLarge = max(numbers) > 255 and not options.ignore
+                if numberTooLarge:
+                    print('Error: found number larger than 255: %d' % max(numbers))
+                    break
+                numberTooSmall = min(numbers) < 0 and not options.ignore
+                if numberTooSmall:
+                    print('Error: found number smaller than 0: %d' % min(numbers))
+                    break
+                oOutput.Line(' '.join(['%02x' % number for number in numbers]))
         if fIn != sys.stdin:
             fIn.close()
         if numberTooLarge:
@@ -157,6 +167,8 @@ https://DidierStevens.com'''
     oParser.add_option('-m', '--man', action='store_true', default=False, help='Print manual')
     oParser.add_option('-o', '--output', type=str, default='', help='Output to file')
     oParser.add_option('-i', '--ignore', action='store_true', default=False, help='Do not generate an error when a number larger than 255 is found')
+    oParser.add_option('-n', '--number', type=int, default=1, help='Minimum number of numbers per line (1 by default)')
+    oParser.add_option('-s', '--signed', action='store_true', default=False, help='Numbers are signed bytes: add 256 if negative')
     (options, args) = oParser.parse_args()
 
     if options.man:
