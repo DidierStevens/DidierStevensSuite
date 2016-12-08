ceb594245b05a8722e791245884cc1393cbcd1c3
DidierStevens <didier.stevens@gmail.com>
2016-12-08 22:45:47 +0100

    20161208a

--- pecheck.py
+++ pecheck.py
@@ -2,8 +2,8 @@
 
 __description__ = 'Tool for displaying PE file info'
 __author__ = 'Didier Stevens'
-__version__ = '0.5.1'
-__date__ = '2016/05/24'
+__version__ = '0.5.2'
+__date__ = '2016/12/01'
 
 """
 
@@ -28,6 +28,8 @@ History:
   2016/05/22: V0.5.1 extended overlay info
   2016/05/23: continued extended overlay info
   2016/05/24: fixed bug GetPEObject
+  2016/08/02: V0.5.2 added signature analysis if pyasn installed
+  2016/12/01: added options -g -D -x -a -S
 
 Todo:
 """
@@ -42,6 +44,10 @@ import signal
 import binascii
 import shlex
 import os
+import re
+
+dumplinelength = 16
+REGEX_STANDARD = '[\x09\x20-\x7E]'
 
 try:
     import pefile
@@ -151,7 +157,50 @@ def NumberOfBytesHumanRepresentation(value):
     else:
         return '%.1f GB' % (float(value) / 1024.0 / 1024.0 / 1024.0)
 
-def SingleFile(filename, signatures, options):
+def Signature(pe):
+    address = pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']].VirtualAddress
+    size = pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']].Size
+
+    if address == 0:
+        print(' No signature')
+        return
+
+    signature = pe.write()[address + 8:address + size]
+
+    try:
+        from pyasn1.codec.der import decoder as der_decoder
+    except:
+        print(' Signature present but error importing pyasn1 module')
+        return
+    try:
+        from pyasn1_modules import rfc2315
+    except:
+        print(' Signature present but error importing pyasn1_modules module')
+        return
+
+    contentInfo, _ = der_decoder.decode(str(signature), asn1Spec=rfc2315.ContentInfo())
+    contentType = contentInfo.getComponentByName('contentType')
+    contentInfoMap = {
+        (1, 2, 840, 113549, 1, 7, 1): rfc2315.Data(),
+        (1, 2, 840, 113549, 1, 7, 2): rfc2315.SignedData(),
+        (1, 2, 840, 113549, 1, 7, 3): rfc2315.EnvelopedData(),
+        (1, 2, 840, 113549, 1, 7, 4): rfc2315.SignedAndEnvelopedData(),
+        (1, 2, 840, 113549, 1, 7, 5): rfc2315.DigestedData(),
+        (1, 2, 840, 113549, 1, 7, 6): rfc2315.EncryptedData()
+    }
+    content, _ = der_decoder.decode(contentInfo.getComponentByName('content'), asn1Spec=contentInfoMap[contentType])
+
+    for line in content.prettyPrint().split('\n'):
+        print(line)
+        oMatch = re.match('( *)value=0x....(.+)', line)
+        if oMatch != None:
+            print(oMatch.groups()[0] + '      ' + repr(binascii.a2b_hex(oMatch.groups()[1])))
+
+#    for idx in range(len(content)):
+#        print(content.getNameByPosition(idx))
+#        print(content.getNameByPosition(idx), content.getComponentByPosition(idx))
+
+def SingleFileInfo(filename, signatures, options):
     pe = GetPEObject(filename)
     raw = pe.write()
     print("PE check for '%s':" % filename)
@@ -162,8 +211,14 @@ def SingleFile(filename, signatures, options):
     print('SHA-512 hash: %s' % hashlib.sha512(raw).hexdigest())
     for section in pe.sections:
         print('%s entropy: %f (Min=0.0, Max=8.0)' % (''.join(filter(lambda c:c != '\0', str(section.Name))), section.get_entropy()))
+
     print('Dump Info:')
     print(pe.dump_info())
+
+    print('Signature:')
+    Signature(pe)
+    print('')
+
     print('PEiD:')
     if signatures == None:
         print('Error: signature database missing')
@@ -213,6 +268,109 @@ def SingleFile(filename, signatures, options):
                     print('  %06x %s:' % (stringdata[0], stringdata[1]))
                     print('   %s' % binascii.hexlify(C2BIP3(stringdata[2])))
                     print('   %s' % repr(stringdata[2]))
+
+class cDumpStream():
+    def __init__(self):
+        self.text = ''
+
+    def Addline(self, line):
+        if line != '':
+            self.text += line + '\n'
+
+    def Content(self):
+        return self.text
+
+def HexDump(data):
+    oDumpStream = cDumpStream()
+    hexDump = ''
+    for i, b in enumerate(data):
+        if i % dumplinelength == 0 and hexDump != '':
+            oDumpStream.Addline(hexDump)
+            hexDump = ''
+        hexDump += IFF(hexDump == '', '', ' ') + '%02X' % ord(b)
+    oDumpStream.Addline(hexDump)
+    return oDumpStream.Content()
+
+def CombineHexAscii(hexDump, asciiDump):
+    if hexDump == '':
+        return ''
+    return hexDump + '  ' + (' ' * (3 * (dumplinelength - len(asciiDump)))) + asciiDump
+
+def HexAsciiDump(data):
+    oDumpStream = cDumpStream()
+    hexDump = ''
+    asciiDump = ''
+    for i, b in enumerate(data):
+        if i % dumplinelength == 0:
+            if hexDump != '':
+                oDumpStream.Addline(CombineHexAscii(hexDump, asciiDump))
+            hexDump = '%08X:' % i
+            asciiDump = ''
+        hexDump+= ' %02X' % ord(b)
+        asciiDump += IFF(ord(b) >= 32, b, '.')
+    oDumpStream.Addline(CombineHexAscii(hexDump, asciiDump))
+    return oDumpStream.Content()
+
+def ExtractStringsASCII(data):
+    regex = REGEX_STANDARD + '{%d,}'
+    return re.findall(regex % 4, data)
+
+def ExtractStringsUNICODE(data):
+    regex = '((' + REGEX_STANDARD + '\x00){%d,})'
+    return [foundunicodestring.replace('\x00', '') for foundunicodestring, dummy in re.findall(regex % 4, data)]
+
+def ExtractStrings(data):
+    return ExtractStringsASCII(data) + ExtractStringsUNICODE(data)
+
+def DumpFunctionStrings(data):
+    return ''.join([extractedstring + '\n' for extractedstring in ExtractStrings(data)])
+
+#Fix for http://bugs.python.org/issue11395
+def StdoutWriteChunked(data):
+    while data != '':
+        sys.stdout.write(data[0:10000])
+        try:
+            sys.stdout.flush()
+        except IOError:
+            return
+        data = data[10000:]
+
+def IfWIN32SetBinary(io):
+    if sys.platform == 'win32':
+        import msvcrt
+        msvcrt.setmode(io.fileno(), os.O_BINARY)
+
+def ParseGetData(expression):
+    terms = expression.split(',')
+    if len(terms) != 2:
+        return None
+    if not terms[0].strip().startswith('0x'):
+        return None
+    if not terms[1].strip().startswith('0x'):
+        return None
+    return (int(terms[0].strip()[2:], 16), int(terms[1].strip()[2:], 16))
+
+def SingleFile(filename, signatures, options):
+    if options.getdata == '':
+        SingleFileInfo(filename, signatures, options)
+    else:
+        pe = GetPEObject(filename)
+        if options.dump:
+            DumpFunction = lambda x:x
+            IfWIN32SetBinary(sys.stdout)
+        elif options.hexdump:
+            DumpFunction = HexDump
+        elif options.asciidump:
+            DumpFunction = HexAsciiDump
+        elif options.strings:
+            DumpFunction = DumpFunctionStrings
+        else:
+            DumpFunction = HexAsciiDump
+        parsed = ParseGetData(options.getdata)
+        if parsed == None:
+            print('Error getdata syntax error: %s' % options.getdata)
+            return
+        StdoutWriteChunked(DumpFunction(pe.get_data(parsed[0], parsed[1])))
 
 def FileContentsStartsWithMZ(filename):
     try:
@@ -315,6 +473,11 @@ def Main():
     oParser.add_option('-e', '--entropy', type=float, default=7.0, help='the minimum entropy value for a file to be listed in scan mode (default 7.0)')
     oParser.add_option('-y', '--yara', help="YARA rule-file, @file or directory to check streams (YARA search doesn't work with -s option)")
     oParser.add_option('--yarastrings', action='store_true', default=False, help='Print YARA strings')
+    oParser.add_option('-g', '--getdata', type=str, default='', help='Get data from the PE file (example 0x1234,0x100)')
+    oParser.add_option('-D', '--dump', action='store_true', default=False, help='perform dump')
+    oParser.add_option('-x', '--hexdump', action='store_true', default=False, help='perform hex dump')
+    oParser.add_option('-a', '--asciidump', action='store_true', default=False, help='perform ascii dump')
+    oParser.add_option('-S', '--strings', action='store_true', default=False, help='perform strings dump')
     (options, args) = oParser.parse_args(GetArgumentsUpdatedWithEnvironmentVariable('PECHECK_OPTIONS'))
 
     if len(args) > 1:
