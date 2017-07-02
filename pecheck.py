85386d8327614c001157ac869d0e26bf62c0f2e9
DidierStevens <didier.stevens@gmail.com>
2017-07-02 13:13:41 +0200

    20170702b

--- pecheck.py
+++ pecheck.py
@@ -2,8 +2,8 @@
 
 __description__ = 'Tool for displaying PE file info'
 __author__ = 'Didier Stevens'
-__version__ = '0.6.0'
-__date__ = '2016/12/12'
+__version__ = '0.7.0'
+__date__ = '2017/07/02'
 
 """
 
@@ -31,6 +31,7 @@ History:
   2016/08/02: V0.5.2 added signature analysis if pyasn installed
   2016/12/01: added options -g -D -x -a -S
   2016/12/12: V0.6.0 added option -o
+  2017/07/02: V0.7.0 added sections (s) to option -o; added # support for option -y
 
 Todo:
 """
@@ -136,17 +137,26 @@ def GetPEObject(filename):
         oPE = pefile.PE(filename)
     return oPE
 
-def YARACompile(fileordirname):
-    dFilepaths = {}
-    if os.path.isdir(fileordirname):
-        for root, dirs, files in os.walk(fileordirname):
-            for file in files:
-                filename = os.path.join(root, file)
-                dFilepaths[filename] = filename
+def YARACompile(ruledata):
+    if ruledata.startswith('#'):
+        if ruledata.startswith('#h#'):
+            rule = binascii.a2b_hex(ruledata[3:])
+        elif ruledata.startswith('#b#'):
+            rule = binascii.a2b_base64(ruledata[3:])
+        else:
+            rule = ruledata[1:]
+        return yara.compile(source=rule)
     else:
-        for filename in ProcessAt(fileordirname):
-            dFilepaths[filename] = filename
-    return yara.compile(filepaths=dFilepaths)
+        dFilepaths = {}
+        if os.path.isdir(ruledata):
+            for root, dirs, files in os.walk(ruledata):
+                for file in files:
+                    filename = os.path.join(root, file)
+                    dFilepaths[filename] = filename
+        else:
+            for filename in ProcessAt(ruledata):
+                dFilepaths[filename] = filename
+        return yara.compile(filepaths=dFilepaths)
 
 def NumberOfBytesHumanRepresentation(value):
     if value <= 1024:
@@ -401,9 +411,190 @@ def Resources(filename, options):
                     print('%4d: %-20s' % (counter, column1))
                 counter += 1
 
+def Sections(filename, options):
+    counter = 1
+    pe = GetPEObject(filename)
+
+    #http://www.hexacorn.com/blog/2016/12/15/pe-section-names-re-visited/
+    dSections = {
+        #The packer/protector/tools section names/keywords
+        '.aspack': 'Aspack packer',
+        '.adata': 'Aspack packer/Armadillo packer',
+        'ASPack': 'Aspack packer',
+        '.ASPack': 'ASPAck Protector',
+        '.boom': 'The Boomerang List Builder (config+exe xored with a single byte key 0x77)',
+        '.ccg': 'CCG Packer (Chinese Packer)',
+        '.charmve': 'Added by the PIN tool',
+        'BitArts': 'Crunch 2.0 Packer',
+        'DAStub': 'DAStub Dragon Armor protector',
+        '!EPack': 'Epack packer',
+        'FSG!': 'FSG packer (not a section name, but a good identifier)',
+        '.gentee': 'Gentee installer',
+        'kkrunchy': 'kkrunchy Packer',
+        '.mackt': 'ImpRec-created section',
+        '.MaskPE': 'MaskPE Packer',
+        'MEW': 'MEW packer',
+        '.MPRESS1': 'Mpress Packer',
+        '.MPRESS2': 'Mpress Packer',
+        '.neolite': 'Neolite Packer',
+        '.neolit': 'Neolite Packer',
+        '.nsp1': 'NsPack packer',
+        '.nsp0': 'NsPack packer',
+        '.nsp2': 'NsPack packer',
+        'nsp1': 'NsPack packer',
+        'nsp0': 'NsPack packer',
+        'nsp2': 'NsPack packer',
+        '.packed': 'RLPack Packer (first section)',
+        'pebundle': 'PEBundle Packer',
+        'PEBundle': 'PEBundle Packer',
+        'PEC2TO': 'PECompact packer',
+        'PECompact2': 'PECompact packer (not a section name, but a good identifier)',
+        'PEC2': 'PECompact packer',
+        'pec1': 'PECompact packer',
+        'pec2': 'PECompact packer',
+        'PEC2MO': 'PECompact packer',
+        'PELOCKnt': 'PELock Protector',
+        '.perplex': 'Perplex PE-Protector',
+        'PESHiELD': 'PEShield Packer',
+        '.petite': 'Petite Packer',
+        '.pinclie': 'Added by the PIN tool',
+        'ProCrypt': 'ProCrypt Packer',
+        '.RLPack': 'RLPack Packer (second section)',
+        '.rmnet': 'Ramnit virus marker',
+        'RCryptor': 'RPCrypt Packer',
+        '.RPCrypt': 'RPCrypt Packer',
+        '.seau': 'SeauSFX Packer',
+        '.sforce3': 'StarForce Protection',
+        '.spack': 'Simple Pack (by bagie)',
+        '.svkp': 'SVKP packer',
+        'Themida': 'Themida Packer',
+        '.Themida': 'Themida Packer',
+        '.taz': 'Some version os PESpin',
+        '.tsuarch': 'TSULoader',
+        '.tsustub': 'TSULoader',
+        '.packed': 'Unknown Packer',
+        'PEPACK!!': 'Pepack',
+        '.Upack': 'Upack packer',
+        '.ByDwing': 'Upack Packer',
+        'UPX0': 'UPX packer',
+        'UPX1': 'UPX packer',
+        'UPX2': 'UPX packer',
+        'UPX!': 'UPX packer',
+        '.UPX0': 'UPX Packer',
+        '.UPX1': 'UPX Packer',
+        '.UPX2': 'UPX Packer',
+        '.vmp0': 'VMProtect packer',
+        '.vmp1': 'VMProtect packer',
+        '.vmp2': 'VMProtect packer',
+        'VProtect': 'Vprotect Packer',
+        '.winapi': 'Added by API Override tool',
+        'WinLicen': 'WinLicense (Themida) Protector',
+        '_winzip_': 'WinZip Self-Extractor',
+        '.WWPACK': 'WWPACK Packer',
+        '.yP': 'Y0da Protector',
+        '.y0da': 'Y0da Protector',
+
+        #List of popular section names
+        '.00cfg': 'Control Flow Guard (CFG) section (added by newer versions of Visual Studio)',
+        '.arch': 'Alpha-architecture section',
+        '.autoload_text': 'cygwin/gcc; the Cygwin DLL uses a section to avoid copying certain data on fork.',
+        '.bindat': 'Binary data (also used by one of the downware installers based on LUA)',
+        '.bootdat': 'section that can be found inside Visual Studio files; contains palette entries',
+        '.bss': 'Uninitialized Data Section',
+        '.BSS': 'Uninitialized Data Section',
+        '.buildid': 'gcc/cygwin; Contains debug information (if overlaps with debug directory)',
+        '.CLR_UEF': '.CLR Unhandled Exception Handler section; see https://github.com/dotnet/coreclr/blob/master/src/vm/excep.h',
+        '.code': 'Code Section',
+        '.cormeta': '.CLR Metadata Section',
+        '.complua': 'Binary data, most likely compiled LUA (also used by one of the downware installers based on LUA)',
+        '.CRT': 'Initialized Data Section  (C RunTime)',
+        '.cygwin_dll_common': "cygwin section containing flags representing Cygwin's capabilities; refer to cygwin.sc and wincap.cc inside Cygwin run-time",
+        '.data': 'Data Section',
+        '.DATA': 'Data Section',
+        '.data1': 'Data Section',
+        '.data2': 'Data Section',
+        '.data3': 'Data Section',
+        '.debug': 'Debug info Section',
+        '.debug$F': 'Debug info Section (Visual C++ version <7.0)',
+        '.debug$P': 'Debug info Section (Visual C++ debug information',
+        '.debug$S': 'Debug info Section (Visual C++ debug information',
+        '.debug$T': 'Debug info Section (Visual C++ debug information',
+        '.drectve ': 'directive section (temporary, linker removes it after processing it; should not appear in a final PE image)',
+        '.didat': 'Delay Import Section',
+        '.didata': 'Delay Import Section',
+        '.edata': 'Export Data Section',
+        '.eh_fram': 'gcc/cygwin; Exception Handler Frame section',
+        '.export': 'Alternative Export Data Section',
+        '.fasm': 'FASM flat Section',
+        '.flat': 'FASM flat Section',
+        '.gfids': 'section added by new Visual Studio (14.0); purpose unknown',
+        '.giats': 'section added by new Visual Studio (14.0); purpose unknown',
+        '.gljmp': 'section added by new Visual Studio (14.0); purpose unknown',
+        '.glue_7t': 'ARMv7 core glue functions (thumb mode)',
+        '.glue_7': 'ARMv7 core glue functions (32-bit ARM mode)',
+        '.idata': 'Initialized Data Section  (Borland)',
+        '.idlsym': 'IDL Attributes (registered SEH)',
+        '.impdata': 'Alternative Import data section',
+        '.itext': 'Code Section  (Borland)',
+        '.ndata': 'Nullsoft Installer section',
+        '.orpc': 'Code section inside rpcrt4.dll',
+        '.pdata': 'Exception Handling Functions Section (PDATA records)',
+        '.rdata': 'Read-only initialized Data Section  (MS and Borland)',
+        '.reloc': 'Relocations Section',
+        '.rodata': 'Read-only Data Section',
+        '.rsrc': 'Resource section',
+        '.sbss': 'GP-relative Uninitialized Data Section',
+        '.script': 'Section containing script',
+        '.shared': 'Shared section',
+        '.sdata': 'GP-relative Initialized Data Section',
+        '.srdata': 'GP-relative Read-only Data Section',
+        '.stab': 'Created by Haskell compiler (GHC)',
+        '.stabstr': 'Created by Haskell compiler (GHC)',
+        '.sxdata': 'Registered Exception Handlers Section',
+        '.text': 'Code Section',
+        '.text0': 'Alternative Code Section',
+        '.text1': 'Alternative Code Section',
+        '.text2': 'Alternative Code Section',
+        '.text3': 'Alternative Code Section',
+        '.textbss': 'Section used by incremental linking',
+        '.tls': 'Thread Local Storage Section',
+        '.tls$': 'Thread Local Storage Section',
+        '.udata': 'Uninitialized Data Section',
+        '.vsdata': 'GP-relative Initialized Data',
+        '.xdata': 'Exception Information Section',
+        '.wixburn': 'Wix section; see https://github.com/wixtoolset/wix3/blob/develop/src/burn/stub/StubSection.cpp',
+        'BSS': 'Uninitialized Data Section  (Borland)',
+        'CODE': 'Code Section (Borland)',
+        'DATA': 'Data Section (Borland)',
+        'DGROUP': 'Legacy data group section',
+        'edata': 'Export Data Section',
+        'idata': 'Initialized Data Section  (C RunTime)',
+        'INIT': 'INIT section (drivers)',
+        'minATL': 'Section that can be found inside some ARM PE files; purpose unknown',
+        'PAGE': 'PAGE section (drivers)',
+        'rdata': 'Read-only Data Section',
+        'sdata': 'Initialized Data Section',
+        'shared': 'Shared section',
+        'Shared': 'Shared section',
+        'testdata': 'section containing test data (can be found inside Visual Studio files)',
+        'text': 'Alternative Code Section',
+    }
+
+    for section in pe.sections:
+        sectionname = ''.join(filter(lambda c:c != '\0', str(section.Name)))
+        description = ''
+        if sectionname in dSections:
+            description = dSections[sectionname]
+        print('%d: %-10s %8d %f %s' % (counter, sectionname, section.SizeOfRawData, section.get_entropy(), description))
+        counter += 1
+
+#        print(dir(section))
+
 def SingleFile(filename, signatures, options):
     if options.overview == 'r':
         Resources(filename, options)
+    elif options.overview == 's':
+        Sections(filename, options)
     elif options.getdata != '':
         pe = GetPEObject(filename)
         if options.dump:
@@ -531,10 +722,10 @@ def Main():
     oParser.add_option('-x', '--hexdump', action='store_true', default=False, help='perform hex dump')
     oParser.add_option('-a', '--asciidump', action='store_true', default=False, help='perform ascii dump')
     oParser.add_option('-S', '--strings', action='store_true', default=False, help='perform strings dump')
-    oParser.add_option('-o', '--overview', type=str, default='', help='Accepted value: r for overview of resources')
+    oParser.add_option('-o', '--overview', type=str, default='', help='Accepted value: r for overview of resources, s for sections')
     (options, args) = oParser.parse_args(GetArgumentsUpdatedWithEnvironmentVariable('PECHECK_OPTIONS'))
 
-    if len(args) > 1 or options.overview != '' and options.overview != 'r':
+    if len(args) > 1 or options.overview != '' and options.overview != 'r' and options.overview != 's':
         oParser.print_help()
         print('')
         print('  Source code put in the public domain by Didier Stevens, no Copyright')
