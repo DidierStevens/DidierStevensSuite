8eb5a115e1be54c52fb2303a762dc18bcb1165c6
Didier Stevens <didier.stevens@gmail.com>
2024-05-18 15:41:44 +0200

    20240518b

--- plugin_msg_summary.py
+++ plugin_msg_summary.py
@@ -64,9 +64,12 @@ class cMSG(cPluginParentOle):
                 self.dAttachments[numberAttachment] = cAttachment(nameAttachment)
                 self.dAttachments[numberAttachment].number = numberAttachment
                 self.dAttachments[numberAttachment].inline = False
+                self.dAttachments[numberAttachment].hidden = False
             if name[1] == '__properties_version1.0':
                 if b'\x03\x00\x14\x37\x06\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00' in stream: # quick & dirty trick to find PidTagAttachFlags with attRenderedInBody 0x00000004
                     self.dAttachments[numberAttachment].inline = True
+                if b'\x0b\x00\xfe\x7f\x06\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00' in stream: # quick & dirty trick to find PidTagAttachmentHidden set to True
+                    self.dAttachments[numberAttachment].hidden = True
             else:
                 found, code = StartsWithGetRemainder(name[1], '__substg1.0_')
                 if found:
@@ -114,12 +117,17 @@ class cMSG(cPluginParentOle):
 
         sha256 = hashlib.sha256(self.data).hexdigest()
         if options.json:
-            jsondata = {'sha256': sha256, 'subject': self.subject, 'date': self.date, 'to': self.to, 'from': self.from_, 'attachments': [{'index': key, 'inline': value.inline, 'longfilename': value.longfilename, 'mimetag': value.mimetag, 'size': len(value.data), 'sha256': hashlib.sha256(value.data).hexdigest(), 'magichex': binascii.b2a_hex(value.data[:4]).decode()} for key, value in self.dAttachments.items()]}
+            jsondata = {'sha256': sha256, 'subject': self.subject, 'date': self.date, 'to': self.to, 'from': self.from_, 'attachments': [{'index': key, 'inline': value.inline, 'hidden': value.hidden, 'longfilename': value.longfilename, 'mimetag': value.mimetag, 'size': len(value.data), 'sha256': hashlib.sha256(value.data).hexdigest(), 'magichex': binascii.b2a_hex(value.data[:4]).decode()} for key, value in self.dAttachments.items()]}
             print(json.dumps(jsondata))
         elif options.jsonattachmentoutput:
             oMyJSONOutput = cMyJSONOutput()
             for index in self.dAttachments.keys():
-                oMyJSONOutput.AddIdItem(index, IFF(self.dAttachments[index].inline, 'inline:', '') + self.dAttachments[index].longfilename, self.dAttachments[index].data)
+                extra = []
+                if self.dAttachments[index].inline:
+                    extra.append('inline')
+                if self.dAttachments[index].hidden:
+                    extra.append('hidden')
+                oMyJSONOutput.AddIdItem(index, IFF(extra != [], ','.join(extra) + ':', '') + self.dAttachments[index].longfilename, self.dAttachments[index].data)
             print(oMyJSONOutput.GetJSON())
         else:
             print('Sample email: sha256 %s' % sha256)
@@ -130,7 +138,7 @@ class cMSG(cPluginParentOle):
             print('From: %s' % self.from_)
             print('Body stream index: %d' % self.bodyStreamIndex)
             for index in self.dAttachments.keys():
-                print('Attachment %d (stream index %d)%s: %s %s %d %s' % (index, self.dAttachments[index].streamIndex, IFF(self.dAttachments[index].inline, ', inline', ''), self.dAttachments[index].longfilename, self.dAttachments[index].mimetag, len(self.dAttachments[index].data), hashlib.sha256(self.dAttachments[index].data).hexdigest()))
+                print('Attachment %d (stream index %d)%s%s: %s %s %d %s' % (index, self.dAttachments[index].streamIndex, IFF(self.dAttachments[index].inline, ', inline', ''), IFF(self.dAttachments[index].hidden, ', hidden', ''), self.dAttachments[index].longfilename, self.dAttachments[index].mimetag, len(self.dAttachments[index].data), hashlib.sha256(self.dAttachments[index].data).hexdigest()))
             if len(self.dURLs) > 0:
                 print('URLs:')
                 for key, value in self.dURLs.items():
